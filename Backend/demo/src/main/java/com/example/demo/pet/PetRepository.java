2d9ff786fda912a3c60cd60a6566ed92770a7862
Madusha Thassara <137921529+thassara@users.noreply.github.com>
2025-02-18 22:36:52 +0530

    add my demo changes

---
+++ Backend/demo/src/main/java/com/example/demo/pet/PetRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.pet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Integer> {
+
+}
---
+++ Backend/demo/src/main/java/com/example/demo/pet/PetService.java
@@ -0,0 +1,14 @@
+package com.example.demo.pet;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PetService {
+
+    Pet add(Pet pet);
+    List<Pet> getPets();
+    Pet update(Pet pet);
+    void delete(Integer id);
+    
+    Optional<Pet> getById(Integer id) throws Exception;
+}
